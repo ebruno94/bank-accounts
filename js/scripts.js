18722165727457b7667c5259df9ebf29a3945794
Ernest Bruno and Justin Lardani <justin@>
2018-01-23 11:37:10 -0800

    Add somewhat complete version of scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,41 @@
 $(document).ready(function(){
+  var currentUser;
+
   var Bank = {
-    accountHolders = [];
-    totalMoney = 0;
+    accountHolders: [],
+    totalMoney: 0,
+    logIn: function(userName, passWord){
+            var booleanFlag = false;
+            var i = 0;
+            this.accountHolders.forEach(function(account){
+              if (account.user === userName && account.pass === passWord){
+                booleanFlag = true;
+              };
+            });
+            return booleanFlag;
+          },
+    removeAccount: function(){
+              var i = 0;
+              this.accountHolders.forEach(function(account){
+                if (this.user === user){
+                  Bank.accountHolders.slice(i, i+1);
+                  $(this).parent().hide("ease");
+                  $("#login").show("ease");
+                  $(".row > input").val("");
+                  Bank.totalMoney -= currentUser.money;
+                  currentUser = undefined;
+                }else{
+                  i++;
+                };
+              });
+            },
+    getCurrentUser: function(user) {
+      this.accountHolders.forEach(function(account) {
+        if (account.user === user) {
+          currentUser = account;
+        };
+      })
+    }
   };
 
   function AccountHolder(name, user, pass, money) {
@@ -12,13 +46,36 @@ $(document).ready(function(){
     this.history = [];
   };
 
-  AccountHolder.prototype.makeDeposit(){
-    this.money += $("#amount").val();
+  AccountHolder.prototype.makeDeposit = function(){
+    this.money += parseInt($("#amount").val());
+    Bank.totalMoney += parseInt($("#amount").val());
   };
 
-  AccountHolder.prototype.makeWithdrawal(){
-    this.money -= $("#amount").val();
+  AccountHolder.prototype.makeWithdrawal = function(){
+    this.money -= parseInt($("#amount").val());
+    Bank.totalMoney -= parseInt($("#amount").val());
   };
+
+
+  $("#login").submit(function(event){
+    event.preventDefault();
+    if (Bank.logIn($("input#username").val(),$("input#password").val())){
+      $("#login").hide("ease");
+      $("#account").show("ease");
+      var user = $("input#username").val();
+      Bank.getCurrentUser(user);
+      $("#balance").text(currentUser.money);
+    } else {
+      alert("Invalid username/password");
+      $("#login > input").val("");
+    };
+    console.log(Bank.totalMoney);
+  });
+
+  $("#createAccount").click(function(event){
+    $("#login").hide("ease");
+    $("#create").show("ease");
+  });
 
   $("#create").submit(function(event){
     event.preventDefault();
@@ -32,8 +89,11 @@ $(document).ready(function(){
       // Add check if username already exists
       Bank.accountHolders.push(new AccountHolder(name, user, pass, money));
       $("form#create, #signin").hide("ease");
-      $("form#account").show("ease");
+      $("#account").show("ease");
       $(".row > input").val("");
+      Bank.getCurrentUser(user);
+      $("#balance").text(currentUser.money);
+      Bank.totalMoney += currentUser.money;
     } else {
       if (money < 0){
         alert("Please enter a positive number!");
@@ -45,8 +105,20 @@ $(document).ready(function(){
     };
   });
 
+  $("#deposit").click(function(){
+    currentUser.makeDeposit();
+    $("#balance").text(currentUser.money);
+  });
+
+  $("#withdraw").click(function(){
+    currentUser.makeWithdrawal();
+    $("#balance").text(currentUser.money);
+  });
+
   $("#logout").click(function(){
-    $(this).parent().parent().hide("ease");
+    $(this).parent().hide("ease");
+    $("#login").show("ease");
     $(".row > input").val("");
+    currentUser = undefined;
   });
 });
